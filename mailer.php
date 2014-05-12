02a1bc50b3298a7c2401790722fd35fbe0148881
cabezud <cabezud@hotmail.com>
2014-05-12 12:31:25 -0700

    mailer and footer

--- mailer.php
+++ mailer.php
@@ -2,9 +2,8 @@
 
     // Only process POST reqeusts.
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        // Get the form fields and remove whitespace.
         $name = strip_tags(trim($_POST["name"]));
-				$name = str_replace(array("\r","\n"),array(" "," "),$name);
+		$name = str_replace(array("\r","\n"),array(" "," "),$name);
         $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
         $phone = $_POST["phone"];
         $message = trim($_POST["message"]);
@@ -13,22 +12,13 @@
         // Check that data was sent to the mailer.
         if ( empty($name) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
             // Set a 400 (bad request) response code and exit.
-            http_response_code(400);
+            header("HTTP/1.0 404 Not Found");//http_response_code(400);
             echo "Oops! There was a problem with this submission. Please complete the form and try again.";
             exit;
         }
 
-        // Set the recipient email address.
-        // FIXME: Update this to your desired email address.
-        //$recipient = "cabez.ud@gmail.com" . ", ";
-        //$recipient .= "c.abezud@gmail.com"
-        //$recipient = "cabez.ud@gmail.com, c.abezud@gmail.com;"
-        $recipient = "cabez.ud@gmail.com"
-
-        // Set the email subject.
+        $recipient = "cabezud@gmail.com, cabezud@yahoo.com"; //"info@sagentmanagement.com, israel@sagentmanagement.com";
         $subject = "Sagent website contact from $name";
-
-        // Build the email content.
         $email_content = "Name: $name\n";
         $email_content .= "Phone: $phone\n";
         $email_content .= "Email: $email\n\n";
@@ -40,17 +30,17 @@
         // Send the email.
         if (mail($recipient, $subject, $email_content, $email_headers)) {
             // Set a 200 (okay) response code.
-            http_response_code(200);
+            header(":", TRUE, 200);//http_response_code(200);
             echo "Thank You! Your message has been sent.";
         } else {
             // Set a 500 (internal server error) response code.
-            http_response_code(500);
+            header("HTTP/1.0 404 Not Found");//http_response_code(500);
             echo "Oops!! Something went wrong and we couldn't send your message.";
         }
 
     } else {
         // Not a POST request, set a 403 (forbidden) response code.
-        http_response_code(403);
+        header("HTTP/1.0 404 Not Found");//http_response_code(403);
         echo "There was a problem with your submission, please try again.";
     }
 
